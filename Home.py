840792cf4551757c6a393d304e05d176925492dc
peter942 <peter@omalley.io>
2023-12-11 19:55:49 +0100

    Copy

--- Home.py
+++ Home.py
@@ -25,7 +25,7 @@ a1, a2 = st.columns([1, 1])
 with a1:  
   st.subheader("Our philosophy")
   st.image("https://banodoco.s3.amazonaws.com/images/philosophy_image.webp",use_column_width='always')
-  st.write("We believe the way to maximise the quality of AI art is to empower open source tool-builders, who in turn empower artists.")
+  st.write("We want to to empower open source explorers and tinkerers to create tools that share their discoveries with the world.")
   st.markdown('<a href="/Philosophy" target="_self">Learn about our philosophy</a>', unsafe_allow_html=True)
        
 with a2:    
--- pages/1_Philosophy.py
+++ pages/1_Philosophy.py
@@ -17,7 +17,7 @@ with footer1:
   st.markdown("- Giulia<span style='color:green'>no</span> Bugiardini",unsafe_allow_html=True)
   st.markdown("- Agnolo di <span style='color:orange'>Do</span>nnino",unsafe_allow_html=True)
   st.markdown("- Jacopo del Tedes<span style='color:blue'>co</span>",unsafe_allow_html=True) 
-  st.write("AI has the potential to equip every individual with equivilent master-level artistic assistance. However, it is also inherently chaotic - each model can be used in vastly different ways to achieve wildly different results - think of each as a prodigy, bursting full of potential that they may or may not reach - depending on the instruction, structure, and inspiration that they receive.")
+  st.write("AI has the potential to equip every individual with equivalent master-level artistic assistance. However, it's also inherently chaotic - each model can be used in vastly different ways to achieve wildly different results. Think of each as a prodigy, full of potential that they may or may not reach - depending on the instruction, inspiration and structure that they receive.")
 
 with footer2: 
   st.write("")  
@@ -25,7 +25,7 @@ with footer2:
   st.image("https://banodoco.s3.amazonaws.com/images/the_lads.webp", use_column_width='always')
 
 
-st.write("At the cutting edge, there are talented, passionate people experimenting with these models - pushing the bounds of what's possible with their ingenuity - discovering control methods, optimal settings, and combining them with other models. However, their work is often lost - inaccessible to those without technical knowledge, or buried in the depths of Github repos.")
+st.write("At the cutting edge, there are talented, passionate people experimenting with these models - pushing the bounds of what's possible with their ingenuity - discovering control methods, optimal settings, and combining them with other models. However, their work is often lost - inaccessible to those without technical knowledge, or buried in the depths of GitHub repos.")
 st.write("We believe that empowering this community of explorers to create tools that any person can use is how to harness the chaotic potential of open source AI - these tools can expose their discoveries to the world, while allowing them to make money from their work.")
 st.write("To do this, our plan is to create a platform and infrastructure that allows them to easily craft tools that bring their discoveries to the world.")
 st.write("We believe that the capabilities of thousands of these tools - each the product of deep domain expertise - will allow artists to create any video they can imagine. As models evolve and become more powerful, these tools will allow entire new art styles, genres, and mediums to be created. Alone each will be powerful, but together they can be the foundation of a new art movement.")
